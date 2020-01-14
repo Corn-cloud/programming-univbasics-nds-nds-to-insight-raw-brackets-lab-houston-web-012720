71196055bb74ee974ad03a8dcab928503473b29f
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-14 01:53:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ def directors_totals(nds)
     
   while movie_index < nds[director_index][:movies].length do 
     binding.pry 
-    totals[director_name] += nds[director_index][:movies][movie_index]
+    totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
    movie_index += 1 
  end 
    director_index += 1
